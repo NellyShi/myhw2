0eeff41b51ed3b9c474ff03d2066c3a747db5f45
Nelly Shigapova <nelly-shig@yandex.ru>
2023-12-11 00:32:21 +0400

    Make task

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int oneMileCost = 20;
+        int ticketPrice = 16589;
+        int result = ticketPrice / oneMileCost;
+        System.out.println("Количество бонусных миль = " + result);
+    }
+}
